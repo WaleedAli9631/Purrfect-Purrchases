8997471d6f8a248485cc632e0a8925bcb80be8d1
cmiller241 <cmiller241@gmail.com>
2022-11-14 17:20:01 -0500

    Cats pulled from DB via Javalin server

--- backend/PurrfectPurchasesBackend/src/main/java/com/revature/project2/Main.java
+++ backend/PurrfectPurchasesBackend/src/main/java/com/revature/project2/Main.java
@@ -5,7 +5,17 @@ import io.javalin.Javalin;
 
 public class Main {
     public static void main(String[] args) {
-        Javalin app = Javalin.create();
+        Javalin app = Javalin.create((config)-> {
+            config.plugins.enableCors((cors)->{
+                cors.add(it -> {
+                    it.defaultScheme = "http";
+                    it.allowHost("localhost:5500");
+                    it.allowHost("127.0.0.1:5500");
+                    it.allowCredentials = true;
+                });
+            });
+        });
+
         AuthenticationController authController = new AuthenticationController();
         authController.mapEndpoints(app);
 
