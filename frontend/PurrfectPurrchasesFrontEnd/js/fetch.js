b5c4fad0bf3c9f67048b8e7e861f08dc381f121c
WaleedAli9631 <waleedali9631@gmail.com>
2022-11-16 13:43:24 -0800

    Delete fetch.js

--- frontend/PurrfectPurrchasesFrontEnd/js/fetch.js
+++
@@ -1,9 +0,0 @@
-fetch('http://127.0.0.1:9090/allcats', {
-    method:'GET',
-    credentials: 'include'  
-  }).then((res) => {
-    return res.json();
-  }).then((responseBody) => {
-    console.log(responseBody);
-});
-
