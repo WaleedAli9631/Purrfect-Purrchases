4e54a99957d2bd9d7420cff13dc46be9a99a3a1d
WaleedAli9631 <waleedali9631@gmail.com>
2022-11-17 14:55:13 -0800

    connected backend to frontend

--- backend/PurrfectPurchasesBackend/src/main/java/com/revature/project2/controller/AccountController.java
+++ backend/PurrfectPurchasesBackend/src/main/java/com/revature/project2/controller/AccountController.java
@@ -5,6 +5,7 @@ import com.revature.project2.model.Account;
 import com.revature.project2.service.AccountService;
 import io.javalin.Javalin;
 
+import java.util.Arrays;
 import java.util.Map;
 
 public class AccountController {
