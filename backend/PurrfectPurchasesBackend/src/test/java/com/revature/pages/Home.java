8dbd5a80fd198171db00c0b424bed09130c1535d
cmiller241 <99040885+cmiller241@users.noreply.github.com>
2022-11-17 20:51:55 -0500

    One more try

--- backend/PurrfectPurchasesBackend/src/test/java/com/revature/pages/Home.java
+++ backend/PurrfectPurchasesBackend/src/test/java/com/revature/pages/Home.java
@@ -3,11 +3,15 @@ import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.support.FindBy;
 import org.openqa.selenium.support.PageFactory;
+import org.openqa.selenium.support.ui.Select;
+
 import java.util.List;
 
 public class Home {
 
-    public Home(WebDriver driver) {PageFactory.initElements(driver,this);}
+    public Home(WebDriver driver) {
+        PageFactory.initElements(driver,this);
+    }
 
     //Navigation Links
     @FindBy(id="homeLink")
@@ -16,6 +20,7 @@ public class Home {
     @FindBy(id="adminPageLink")
     public WebElement AdminLink;
 
+    //Content Information
     @FindBy(className="BreedDeclaration")
     public List<WebElement> BreedList;
 
@@ -29,12 +34,41 @@ public class Home {
     //Cat Filter Elements
     @FindBy(id="breedSelect")
     public WebElement BreedDropDown;
+    public Select getBreedOptions() {
+        return new Select(BreedDropDown);
+    }
+    public void setBreedOption(String value) {
+        getBreedOptions().selectByVisibleText(value);
+    }
+    public String getBreedOption() {
+        return getBreedOptions().getFirstSelectedOption().getText();
+    }
 
     @FindBy(id="ageSelect")
     public WebElement AgeDropDown;
 
+    public Select getAgeOptions() {
+        return new Select(AgeDropDown);
+    }
+    public void setAgeOption(String value) {
+        getAgeOptions().selectByVisibleText(value);
+    }
+    public String getAgeOption() {
+        return getAgeOptions().getFirstSelectedOption().getText();
+    }
+
     @FindBy(id="genderSelect")
     public WebElement GenderDropDown;
+
+    public Select getGenderOptions() {
+        return new Select(GenderDropDown);
+    }
+    public void setGenderOption(String value) {
+        getGenderOptions().selectByVisibleText(value);
+    }
+    public String getGenderOption() {
+        return getGenderOptions().getFirstSelectedOption().getText();
+    }
 
     @FindBy(id="filterCatButton")
     public WebElement FilterButton;
